578e5d8e45c064e3e74eb376393a71cca9968873
Yutian Li <hotpxless@gmail.com>
2017-08-02 22:00:44 -0700

    [master] Rename.

--- minpy/__init__.py
+++ minpy/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from .core import rewrite
+from .core import jit
 from .decorators import atomic
 from .segment import segment, test_segment
