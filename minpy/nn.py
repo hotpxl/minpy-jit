a97b677a84eadad934551dadd98cd2b43311cec6
Yutian Li <hotpxless@gmail.com>
2017-08-02 21:38:57 -0700

    [classmethod] Code review, decorator style instance method.

--- minpy/nn.py
+++ minpy/nn.py
@@ -1,183 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+
 import ast
 import inspect
 import textwrap
-from .segment import do_segment
+import functools
+
+from . import core
+
+# CR(gaiyu): To my understanding, you are depending on inheritance
+# to make sure `compile` gets called. And `compile` takes a class
+# and turns the forward method into a jittable free function. It
+# would not work if either the user did not inherit from Module or
+# the user did not call the function forward. To make it more
+# general, I suggest you provide a decorator that lifts the method
+# to a free function. So the user can annotate whatever function
+# they want, and don't have to worry about inheritance.
+#
+# Something like this:
+# def decorator(func):
+#     get source
+#     dedent func
+#     compile func (common code)
+#     def wrapped_fn(*args, **kwargs):
+#         return compiled_fn(*args, **kwargs)
+#     return wrapped_fn
+#
+# XCR(yutian): I am not sure whether it is feasible to lift a bound method
+# to free function via a decorator. For example:
+#
+# def decorator(f):
+#     ...
+#
+# class C:
+#     @decorator
+#     def wrapped_method(self):
+#         return self.attr
+#
+# When received by `decorator`, `wrapped_method` is actually "free", meaning that
+# it does not contain any reference to `self`. However, in order to lift a bound method,
+# I must be able to access `self` and put references to all attributes of `self`
+# that are used in `wrapped_method` in an auxiliary namespace. In pseudocode:
+#
+# namespace = {}
+# used_attrs = search_for_used_attrs_via_ast(f)
+# for attr in used_attrs:
+#     namespace[attr] = getattr(self, attr) # `self` is unavailable!
+# compile_in_namespace(f, namespace)
+#
+# But I think it is possible to remove the constraint that only `forward` can be lifted.
+#
+# XCR(gaiyu): I think you probably have misconceptions towards Python
+# instance methods. The method definition itself is never "bound". You
+# cannot access instance variables WITHOUT going through `self`. In
+# fact, in Python implementation, the "binding" process happens at
+# access time (when you say "obj.func"). So the definition itself
+# SHOULD look no different than a normal free function. It is very
+# different from a C++/Java instance method definition.  I have taken
+# the liberty to rewrite it in terms of existing functions. Check if
+# this achieves the intended behavior.
+
+_reentrance = False
 
 
-class Module:
-    F = __import__('mxnet').ndarray
+def jit_instance_method(func):
+    global _reentrance
+    if _reentrance:
+        return func
+    _reentrance = True
+    source_code = textwrap.dedent(inspect.getsource(func))
+    function_ast = ast.parse(source_code, mode='exec')
+    # logic to modify source, share workflow logic with jit, here we just print the ast
+    print(core.pretty_print(function_ast))
+    free_function = core.evaluate_function_definition(function_ast,
+                                                      func.__globals__, [], [])
 
-    def __init__(self):
-        super().__setattr__('_modules', set())
-        super().__setattr__('_parameters', set())
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        return free_function(*args, **kwargs)
 
-    # CR(gaiyu): To my understanding, you are depending on inheritance
-    # to make sure `compile` gets called. And `compile` takes a class
-    # and turns the forward method into a jittable free function. It
-    # would not work if either the user did not inherit from Module or
-    # the user did not call the function forward. To make it more
-    # general, I suggest you provide a decorator that lifts the method
-    # to a free function. So the user can annotate whatever function
-    # they want, and don't have to worry about inheritance.
-    #
-    # Something like this:
-    # def decorator(func):
-    #     get source
-    #     dedent func
-    #     compile func (common code)
-    #     def wrapped_fn(*args, **kwargs):
-    #         return compiled_fn(*args, **kwargs)
-    #     return wrapped_fn
-    #
-    # XCR(yutian): I am not sure whether it is feasible to lift a bound method
-    # to free function via a decorator. For example:
-    # 
-    # def decorator(f):
-    #     ...
-    #
-    # class C:
-    #     @decorator
-    #     def wrapped_method(self):
-    #         return self.attr
-    #
-    # When received by `decorator`, `wrapped_method` is actually "free", meaning that
-    # it does not contain any reference to `self`. However, in order to lift a bound method,
-    # I must be able to access `self` and put references to all attributes of `self`
-    # that are used in `wrapped_method` in an auxiliary namespace. In pseudocode:
-    #
-    # namespace = {}
-    # used_attrs = search_for_used_attrs_via_ast(f)
-    # for attr in used_attrs:
-    #     namespace[attr] = getattr(self, attr) # `self` is unavailable!
-    # compile_in_namespace(f, namespace)
-    #
-    # But I think it is possible to remove the constraint that only `forward` can be lifted.
-
-    def __setattr__(self, attr, value):
-        assert attr not in self._modules and \
-          attr not in self._parameters
-        if isinstance(value, Module):
-            compiled = value.compile()
-            super().__setattr__(attr, compiled)
-            self._modules.add(attr)
-        elif isinstance(value, self.F.NDArray):
-            self._parameters.add(attr)
-            super().__setattr__(attr, value)
-        else:
-            # attributes other than modules and parameters are permitted
-            # but `self.compile` will raise an exception if it detects these attributes in `self.forward`
-            super().__setattr__(attr, value)
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def forward(self):
-        """ To be overridden. These assumptions applies:
-        - No `*args` or `**kwargs`
-        - No `setattr` of attributes in `self._modules` or `self._parameters`
-        - No `setattr` or `getattr` of attrtibutes other than modules and parameters
-
-        Parameters
-        ----------
-
-        """
-        raise NotImplementedError()
-
-    def compile(self):
-        """ Convert `self.forward` to static function and segment the converted function.
-        """
-        # CR(gaiyu): the main logic here is:
-        # 1. get source code
-        # 2. pre processing (dedent)
-        # 3. compile
-        # At least part 3 you can use common code. Or I can hook that up later.
-        source = inspect.getsource(self.forward)
-        source = textwrap.dedent(source)
-        node = ast.parse(source)
-        f_def = node.body[0]
-        self_id = f_def.args.args[0].arg
-        arg_id_list = []
-        namespace = {}
-
-        class AttributeTransformer(ast.NodeTransformer):
-            # TODO (gaiyu) handle attributes other than parameters
-            def __init__(self, module):
-                super().__init__()
-                self._module = module
-
-            def visit_Attribute(self, node):
-                self.generic_visit(node)
-                if isinstance(node.value,
-                              ast.Name) and node.value.id == self_id:
-                    if node.attr == 'F':  # mxnet.ndarray
-                        transformed = ast.Name(id='F', ctx=ast.Load())
-                        namespace['F'] = self._module.F
-                    elif node.attr in self._module._parameters:
-                        arg_id = '__arg%s' % node.attr
-                        arg_id_list.append(arg_id)
-                        arg = getattr(self._module, node.attr)
-                        namespace[arg_id] = arg
-                        transformed = ast.Name(
-                            id=arg_id,
-                            ctx=ast.Load())  # node.ctx must be ast.Load
-                    elif node.attr in self._module._modules:
-                        module_id = '__module%s' % node.attr
-                        module = getattr(self._module, node.attr)
-                        namespace[module_id] = module
-                        transformed = ast.Name(
-                            id=module_id,
-                            ctx=ast.Load())  # node.ctx must be ast.Load
-                    else:
-                        raise Exception(
-                            'Attributes other than parameters and modules: %s'
-                            % node.attr)
-                    transformed = ast.copy_location(transformed, node)
-                    return transformed
-                return node
-
-        node = AttributeTransformer(self).visit(node)
-        f_def.args.args.pop(0)
-        if len(f_def.args.args) > 0:
-            locator = f_def.args.args[-1]  # arguments on multiple lines
-        else:
-            locator = f_def
-        for arg_id in arg_id_list:
-            arg_node = ast.arg(arg=arg_id)
-            arg_node = ast.copy_location(arg_node, locator)
-            f_def.args.args.append(arg_node)
-            default_node = ast.Name(id=arg_id, ctx=ast.Load())
-            default_node = ast.copy_location(default_node, locator)
-            f_def.args.defaults.append(default_node)
-        '''
-        always_true = lambda _: True
-        node = do_segment(node, always_true, always_true, True)
-        '''
-        exec(compile(node, 'ast', 'exec'), namespace)
-        self.__class__.__call__ = staticmethod(namespace['forward'])
-        return namespace['forward']
-
-
-class Linear(Module):
-    def __init__(self, in_features, out_features):
-        """ Affine transformation.
-
-        Parameters
-        ----------
-        in_features: dimension of input data
-        out_features: dimension of output data
-        """
-
-        super().__init__()
-        self._weight = self.F.zeros((in_features, out_features))
-        self._bias = self.F.zeros((out_features, ))
-
-    def forward(self, data):
-        """ Forward.
-
-        Parameters
-        ----------
-        data: NDArray
-        """
-
-        return self.F.dot(data, self._weight) + self._bias
+    _reentrance = False
+    return wrapped_func
