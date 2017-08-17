21a489f5bedf63401239751f0b099fcd2d28980a
Yutian Li <hotpxless@gmail.com>
2017-08-17 16:46:03 -0700

    [experimental-rules] Also add mxnet functions.

--- minpy/fuse.py
+++ minpy/fuse.py
@@ -7,11 +7,13 @@ from __future__ import print_function
 import ast
 import mxnet
 
+_ndarray_funcs = vars(mxnet.nd).values()
+
 
 class NodeTransformer(ast.NodeTransformer):
     def visit(self, node):
         # Always recurse and then call corresponding method.
-        method = 'visit_' + node.__class__.__name__
+        method = 'visit_' + type(node).__name__
         node = self.generic_visit(node)
         return getattr(self, method, lambda x: x)(node)
 
@@ -33,10 +35,11 @@ class NodeTransformer(ast.NodeTransformer):
         return node
 
     def visit_Call(self, node):
-        if hasattr(node, 'ref') and getattr(
-                node.ref, '__minpy_atomic', False) and all(
-                    map(lambda x: getattr(x, 'fuse', False), node.args)):
-            node.fuse = True
+        if hasattr(node, 'ref'):
+            if getattr(node.ref, '__minpy_atomic',
+                       False) or node.ref in _ndarray_funcs:
+                if all(map(lambda x: getattr(x, 'fuse', False), node.args)):
+                    node.fuse = True
         return node
 
 
