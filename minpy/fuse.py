220d083b13be48246260ab8e9190f9139856ce77
Yutian Li <hotpxless@gmail.com>
2017-08-17 17:06:55 -0700

    [experimental-rules] Not dealing with multiple assignment now.

--- minpy/fuse.py
+++ minpy/fuse.py
@@ -23,29 +23,52 @@ class NodeTransformer(ast.NodeTransformer):
         node = self.generic_visit(node)
         return getattr(self, method, lambda x: x)(node)
 
+    def get_fuse(self, node):
+        return getattr(node, 'fuse', False)
+
     def visit_Name(self, node):
         if issubclass(getattr(node, 'type', object), mxnet.nd.NDArray):
             node.fuse = True
         return node
 
     def visit_BinOp(self, node):
-        if getattr(node.left, 'fuse', False) and getattr(
-                node.right, 'fuse', False):
+        if self.get_fuse(node.left) and self.get_fuse(node.right):
             node.fuse = True
         return node
 
     def visit_Tuple(self, node):
-        if isinstance(node.ctx, ast.Load) and all(
-                map(lambda x: getattr(x, 'fuse', False), node.elts)):
+        if isinstance(
+                node.ctx,
+                ast.Load) and all(map(lambda x: self.get_fuse(x), node.elts)):
             node.fuse = True
         return node
 
     def visit_Call(self, node):
+        # what if the function itself is a complicated attribute? even
+        # dynamic?
+        # func_dict[function_name](arguments) like this?
+        # how to deal with this formally?
         if hasattr(node, 'ref'):
             if getattr(node.ref, '__minpy_atomic',
                        False) or node.ref in _ndarray_funcs:
-                if all(map(lambda x: getattr(x, 'fuse', False), node.args)):
+                if all(map(lambda x: self.get_fuse(x), node.args)):
                     node.fuse = True
+        return node
+
+    # statements below
+    def visit_Assign(self, node):
+        # TODO(yutian) multiple targets
+        if len(node.targets) == 1 and isinstance(
+                node.targets[0], ast.Name) and self.get_fuse(node.value):
+            node.fuse = True
+        return node
+
+    # Why do we need an explicit expr now we have dealt with
+    # expressions before?  Because here it means a statement with a
+    # single expression.
+    def visit_Expr(self, node):
+        if self.get_fuse(node.value):
+            node.fuse = True
         return node
 
 
