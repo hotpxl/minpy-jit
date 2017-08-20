d326dafd573b01dce89b6df8f9b470f18d0c13af
Yutian Li <hotpxless@gmail.com>
2017-08-19 23:43:32 -0700

    [experimental-rules] Plugging in existing i/o inferer.

--- minpy/fuse.py
+++ minpy/fuse.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import ast
+
 import mxnet
 
 _ndarray_funcs = vars(mxnet.nd).values()
@@ -104,7 +105,6 @@ class FuseAnalyzer(ast.NodeTransformer):
     # Statements below.
 
     def visit_Assign(self, node):
-        # TODO(yutian) multiple targets
         if len(node.targets) == 1 and isinstance(
                 node.targets[0], ast.Name) and get_fuse(node.value):
             node.fuse = True
@@ -119,9 +119,124 @@ class FuseAnalyzer(ast.NodeTransformer):
         return node
 
 
+def infer_inputs_and_outputs_given_nodes(nodes):
+    """Given a/a list of ast-node, infer the input and output variables
+    Parameters
+    ----------
+    nodes: a single node or a lsit of nodes
+
+    Returns
+    -------
+    ins: the input variable names
+    outs: the output variable names
+    """
+
+    def infer_inputs_and_outputs_given_node(node):
+        """Given a ast-node, infer the input and output variables
+        The node should be either assign-statement or expression
+
+        Parameters
+        ----------
+        node: a single node
+
+        Returns
+        -------
+        ins: the input variable names
+        outs: the output variable names
+        """
+        if isinstance(node, ast.Assign):
+            # get inputs from its value expression
+            ins, _ = infer_inputs_and_outputs_given_node(node.value)
+            # treat all the targets as outputs
+            outs = collect_names_given_exprs(node.targets)
+            return ins, outs
+        elif isinstance(node, (ast.expr, ast.Expr)):
+            return collect_names_given_exprs(node), set([])
+        else:
+            raise TypeError(
+                'Type {} not handled yet in inputs and outputs inference'.
+                format(type(node).__name__))
+
+    def collect_names_given_exprs(expr):
+        """Given a ast-node, infer the input variables
+        As expression cannot define a new variable, output is not inferred
+
+        Parameters
+        ----------
+        expr: an expression
+
+        Returns
+        -------
+        ins: the input variable names
+
+        TODO:
+          - handle the slice object
+          - need to know the actual type of the left operand of attribute
+            - if it's module or class, then return []
+        """
+        if isinstance(expr, list):
+            return set.union(*map(collect_names_given_exprs,
+                                  expr)) if expr else set()
+        elif isinstance(expr, ast.Call):
+            return collect_names_given_exprs(expr.args)
+        elif isinstance(expr, ast.Expr):
+            return collect_names_given_exprs(expr.value)
+        elif isinstance(expr, ast.BinOp):
+            return collect_names_given_exprs([expr.left, expr.right])
+        elif isinstance(expr, ast.UnaryOp):
+            return collect_names_given_exprs(expr.operand)
+        elif isinstance(expr, ast.Tuple):
+            return collect_names_given_exprs(expr.elts)
+        elif isinstance(expr, ast.Attribute):
+            # Assumption: left operand is a Name
+            assert isinstance(expr.expr, ast.Name)
+            return set([expr.expr.id + "." + expr.attr])
+        elif isinstance(expr, ast.Subscript):
+            # Assumption: left operand is a Name
+            assert isinstance(expr.expr, ast.Name)
+            return set([expr.expr.id + "_subscript_"])
+        elif isinstance(expr, ast.Name):
+            return set([expr.id])
+        elif isinstance(expr, (ast.Num, ast.Str, ast.Bytes)):
+            return set()
+
+        raise TypeError('{} not handled yet in inference of inputs'.format(
+            type(expr).__name__))
+
+    if isinstance(nodes, list):
+        ins = []
+        outs = []
+        for node in nodes:
+            sub_ins, sub_outs = infer_inputs_and_outputs_given_node(node)
+            ins += [x for x in sub_ins if x not in outs]
+            outs += sub_outs
+        return [e for e in (set(ins))], [e for e in set(outs)]
+    else:
+        return infer_inputs_and_outputs_given_node(nodes)
+
+
+class EnvironmentAnalyzer(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        # Merge consecutive statements.
+        body = node.body
+        i = j = 0
+        while j < len(body):
+            if not get_fuse(body[j]):
+                if i < j:
+                    print(i, j)
+                    print(infer_inputs_and_outputs_given_nodes(body[i:j]))
+                i = j + 1
+            j += 1
+        if i < j:
+            print(i, j)
+            print(infer_inputs_and_outputs_given_nodes(body[i:j]))
+        return node
+
+
 def fuse(function_ast):
     function_ast = StaticAnalyzer().visit(function_ast)
     function_ast = FuseAnalyzer().visit(function_ast)
+    function_ast = EnvironmentAnalyzer().visit(function_ast)
     # get consec stmt blablabla
     # get input/output blabla
     return function_ast
